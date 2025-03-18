c942fe405ab783f6e60205202feefacea4256f60
Yerzhan Zhamashev <yerzhan@novel.systems>
2025-03-18 18:31:12 +0200

    Add dotenv support to load environment variables in main.py

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import logging
 import sys
 from os import getenv
 
+# Add dotenv import
+from dotenv import load_dotenv
+
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
